bac201154b5aaddc886aa07fb0b631e37d339b1d
Alex Titomirov <burjui777@yandex.ru>
2024-09-15 15:17:49 +0300

    Homework_3 done

---
+++ homework/alexey_titomirov/homework_3/task_1.py
@@ -0,0 +1,6 @@
+a = 456
+b = 123
+
+difference = a - b
+summ = a + b
+product = a * b
---
+++ homework/alexey_titomirov/homework_3/task_3.py
@@ -0,0 +1,7 @@
+import math
+
+a = 128
+b = 256
+
+arithmetic_mean = (a + b) / 2
+geometric_mean = math.sqrt(a * b)
