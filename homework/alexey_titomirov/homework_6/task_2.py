2dd4d35de2d5e77c7f483689b4e639b0af430263
Alex Titomirov <burjui777@yandex.ru>
2025-01-22 14:07:07 +0300

    homework_6 fixed

--- homework/alexey_titomirov/homework_6/task_2.py
+++ homework/alexey_titomirov/homework_6/task_2.py
@@ -1,10 +1,9 @@
 for number in range(1, 101):
-    if (number % 3) != 0 and (number % 5) != 0:
-        print(number)
-    if (number % 3) == 0:
-        if (number % 5) == 0:
-            print('FuzzBuzz')
-        else:
-            print('Fuzz')
-    if (number % 5) == 0 and (number % 3) != 0:
+    if (number % 3) == 0 and (number % 5) == 0:
+        print('FuzzBuzz')
+    elif (number % 5) == 0:
         print('Buzz')
+    elif (number % 3) == 0:
+        print('Fuzz')
+    else:
+        print(number)
