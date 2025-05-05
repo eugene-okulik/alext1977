f43570eb0f79e2e755b991c3d0d28ea183a5179f
Alexey Titomirov <burjui777@yandex.ru>
2025-05-05 21:06:27 +0300

    homework 8 is done

---
+++ homework/alexey_titomirov/homework_8/task_2.py
@@ -0,0 +1,21 @@
+import sys
+
+def fibonacci_numbers():
+    a, b = 0, 1
+    while True:
+        yield a
+        a, b = b, a + b
+
+sys.set_int_max_str_digits(0)
+count = 1
+for number in fibonacci_numbers():
+    if count == 5:
+        print(number)
+    elif count == 200:
+        print(number)
+    elif count == 1000:
+        print(number)
+    elif count == 100000:
+        print(number)
+        break
+    count += 1
