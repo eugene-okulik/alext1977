245c5b80d38048d379c492892a7c3c7e90f54591
Alexey Titomirov <burjui777@yandex.ru>
2026-08-24 14:07:28 +0300

    homework 15 is fixed

--- homework/alexey_titomirov/homework_15/task_1.py
+++ homework/alexey_titomirov/homework_15/task_1.py
@@ -27,9 +27,6 @@ values = [('Python Testing', student_table_id),
           ('Java Testing', student_table_id),
           ('JavaScript Testing', student_table_id)]
 cursor.executemany(query, values)
-books_table_id_3 = cursor.lastrowid
-books_table_id_2 = books_table_id_3 - 1
-books_table_id_1 = books_table_id_3 - 2
 
 db.commit()
 
@@ -40,8 +37,9 @@ values = ('MyWorkGroup', 'feb 2026', 'nov 2026')
 cursor.execute(query, values)
 group_table_id = cursor.lastrowid
 
-query = f"UPDATE students SET group_id = {group_table_id} WHERE id = {student_table_id}"
-cursor.execute(query)
+query = f"UPDATE students SET group_id = (%s) WHERE id = (%s)"
+values = [group_table_id, student_table_id]
+cursor.execute(query, values)
 
 db.commit()
 
@@ -51,41 +49,40 @@ query = "INSERT INTO subjects (title) VALUES (%s)"
 values = [('Math2026',),
           ('History2026',),
           ('Sport2026',)]
-cursor.executemany(query, values)
-subject_table_id_3 = cursor.lastrowid
-subject_table_id_2 = subject_table_id_3 - 1
-subject_table_id_1 = subject_table_id_3 - 2
+
+subject_table_id = []
+for i in range(len(values)):
+    cursor.execute(query, values[i])
+    subject_table_id.append(cursor.lastrowid)
 
 db.commit()
 
 # 5.Создайте по два занятия для каждого предмета (lessons)
 
 query = "INSERT INTO lessons (title, subject_id) VALUES (%s, %s)"
-values = [('lesson1', subject_table_id_1),
-          ('lesson2', subject_table_id_1),
-          ('lesson1', subject_table_id_2),
-          ('lesson2', subject_table_id_2),
-          ('lesson1', subject_table_id_3),
-          ('lesson2', subject_table_id_3)]
-cursor.executemany(query, values)
-lesson_table_id_6 = cursor.lastrowid
-lesson_table_id_5 = lesson_table_id_6 - 1
-lesson_table_id_4 = lesson_table_id_6 - 2
-lesson_table_id_3 = lesson_table_id_6 - 3
-lesson_table_id_2 = lesson_table_id_6 - 4
-lesson_table_id_1 = lesson_table_id_6 - 5
+values = [('lesson1', subject_table_id[0]),
+          ('lesson2', subject_table_id[0]),
+          ('lesson1', subject_table_id[1]),
+          ('lesson2', subject_table_id[1]),
+          ('lesson1', subject_table_id[2]),
+          ('lesson2', subject_table_id[2])]
+
+lesson_table_id = []
+for i in range(len(values)):
+    cursor.execute(query, values[i])
+    lesson_table_id.append(cursor.lastrowid)
 
 db.commit()
 
 # 6.Поставьте своему студенту оценки (marks) для всех созданных вами занятий
 
 query = "INSERT INTO marks (value, lesson_id, student_id) VALUES (%s, %s, %s)"
-values = [('10', lesson_table_id_6, student_table_id),
-          ('9', lesson_table_id_5, student_table_id),
-          ('8', lesson_table_id_4, student_table_id),
-          ('7', lesson_table_id_3, student_table_id),
-          ('6', lesson_table_id_2, student_table_id),
-          ('5', lesson_table_id_1, student_table_id)]
+values = [('10', lesson_table_id[5], student_table_id),
+          ('9', lesson_table_id[4], student_table_id),
+          ('8', lesson_table_id[3], student_table_id),
+          ('7', lesson_table_id[2], student_table_id),
+          ('6', lesson_table_id[1], student_table_id),
+          ('5', lesson_table_id[0], student_table_id)]
 cursor.executemany(query, values)
 
 db.commit()
@@ -93,14 +90,16 @@ db.commit()
 # Получите информацию из базы данных:
 # 1.Все оценки студента
 
-query = f"SELECT * FROM marks WHERE student_id = {student_table_id}"
-cursor.execute(query)
+query = f"SELECT * FROM marks WHERE student_id = (%s)"
+values = [student_table_id, ]
+cursor.execute(query, values)
 print(cursor.fetchall())
 
 # 2.Все книги, которые находятся у студента
 
-query = f"SELECT * FROM books WHERE taken_by_student_id = {student_table_id}"
-cursor.execute(query)
+query = f"SELECT * FROM books WHERE taken_by_student_id = (%s)"
+values = [student_table_id, ]
+cursor.execute(query, values)
 print(cursor.fetchall())
 
 # 3.Для вашего студента выведите всё, что о нем есть в базе: группа, книги, оценки с названиями занятий
@@ -111,8 +110,9 @@ JOIN books b ON s.id = b.taken_by_student_id
 JOIN marks m  ON s.id = m.student_id
 JOIN lessons l ON m.lesson_id = l.id
 JOIN subjects s2 ON l.subject_id = s2.id
-WHERE s.id = {student_table_id}"""
-cursor.execute(query)
+WHERE s.id = (%s)"""
+values = [student_table_id, ]
+cursor.execute(query, values)
 print(cursor.fetchall())
 
 db.close()
