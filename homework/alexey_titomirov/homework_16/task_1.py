665b38a30edd0c8fa6242c5541b37fbe81b516f3
Alexey Titomirov <burjui777@yandex.ru>
2026-08-27 12:56:35 +0300

    homework 16 is fixed linter error2

--- homework/alexey_titomirov/homework_16/task_1.py
+++ homework/alexey_titomirov/homework_16/task_1.py
@@ -27,11 +27,11 @@ with open(eugene_file_path, newline='') as csv_file:
 
 for i in range(len(data)):
     query = """SELECT s.name, s.second_name, g.title, b.title, s2.title, l.title, m.value  
-    FROM students s JOIN `groups` g ON s.group_id = g.id
-    JOIN books b ON s.id = b.taken_by_student_id
-    JOIN marks m  ON s.id = m.student_id
-    JOIN lessons l ON m.lesson_id = l.id
-    JOIN subjects s2 ON l.subject_id = s2.id
+    FROM students s JOIN `groups` g ON s.group_id = g.id 
+    JOIN books b ON s.id = b.taken_by_student_id 
+    JOIN marks m  ON s.id = m.student_id 
+    JOIN lessons l ON m.lesson_id = l.id 
+    JOIN subjects s2 ON l.subject_id = s2.id 
     WHERE s.name = (%s) 
     AND s.second_name = (%s) 
     AND g.title = (%s) 
